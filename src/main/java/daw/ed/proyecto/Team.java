81e873c6ca2cb3955b7567e85bc8014efb3862d0
Shaheer Shahzad <shaheer19962012@gmail.com>
2015-05-21 09:30:46 +0200

    Actualizado pom y la carpeta src

---
+++ src/main/java/daw/ed/proyecto/Team.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.ed.proyecto;
+
+/**
+ *
+ * @author shaheer
+ */
+public class Team {
+
+    private String nm;
+    private int pj;
+    private int pg;
+    private int pe;
+    private int pp;
+    private int gf;
+    private int gc;
+    private int puntos;
+
+    public Team() {
+
+    }
+
+    public Team(String nm, int pj, int pg, int pe, int pp, int gf, int gc, int puntos) {
+        this.nm = nm;
+        this.pj = pj;
+        this.pg = pg;
+        this.pe = pe;
+        this.pp = pp;
+        this.gf = gf;
+        this.gc = gc;
+        this.puntos = puntos;
+    }
+
+    public String getNm() {
+        return nm;
+    }
+
+    public void setNm(String nm) {
+        this.nm = nm;
+    }
+
+    public int getPj() {
+        return pj;
+    }
+
+    public void setPj(int pj) {
+        this.pj = pj;
+    }
+
+    public int getPg() {
+        return pg;
+    }
+
+    public void setPg(int pg) {
+        this.pg = pg;
+    }
+
+    public int getPe() {
+        return pe;
+    }
+
+    public void setPe(int pe) {
+        this.pe = pe;
+    }
+
+    public int getPp() {
+        return pp;
+    }
+
+    public void setPp(int pp) {
+        this.pp = pp;
+    }
+
+    public int getGf() {
+        return gf;
+    }
+
+    public void setGf(int gf) {
+        this.gf = gf;
+    }
+
+    public int getGc() {
+        return gc;
+    }
+
+    public void setGc(int gc) {
+        this.gc = gc;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+}

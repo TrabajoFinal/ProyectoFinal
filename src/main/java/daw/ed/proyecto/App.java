6c32ee0f57a571192cb5602c2579d75b9d217c4d
Shaheer Shahzad <shaheer19962012@gmail.com>
2015-05-28 20:28:08 +0200

    Equipos reales

--- src/main/java/daw/ed/proyecto/App.java
+++ src/main/java/daw/ed/proyecto/App.java
@@ -22,26 +22,26 @@ public class App {
         Spark.staticFileLocation("/public");
         final ArrayList<Team> teams = new ArrayList<>();
 
-        teams.add(new Team("mi nombre", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre2", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre3", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("FC Barcelona", "38", "5", "1", "0", "45", "54", "87"));
-        teams.add(new Team("mi nombre", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre2", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre3", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("FC Barcelona", "38", "5", "1", "0", "45", "54", "87"));
-        teams.add(new Team("mi nombre", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre2", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre3", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("FC Barcelona", "38", "5", "1", "0", "45", "54", "87"));
-        teams.add(new Team("mi nombre", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre2", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre3", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("FC Barcelona", "38", "5", "1", "0", "45", "54", "87"));
-        teams.add(new Team("mi nombre", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre2", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre3", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
-        teams.add(new Team("mi nombre3", "pj", "pg", "pe", "pp", "gj", "gc", "puntos"));
+        teams.add(new Team("FC Barcelona", "38", "30", "4", "4", "110", "21", "94"));
+        teams.add(new Team("Real Madrid CF", "38", "30", "2", "6", "118", "38", "92"));
+        teams.add(new Team("Atletico", "38", "23", "9", "6", "67", "29", "78"));
+        teams.add(new Team("Valencia CF", "38", "22", "11", "5", "70", "32", "77"));
+        teams.add(new Team("Sevilla", "38", "23", "7", "8", "71", "45", "76"));
+        teams.add(new Team("Villareal", "38", "16", "12", "10", "48", "37", "60"));
+        teams.add(new Team("Athletic Bilbao", "38", "15", "10", "13", "42", "41", "55"));
+        teams.add(new Team("Celta de Vigo", "38", "13", "12", "13", "47", "44", "51"));
+        teams.add(new Team("Málaga", "38", "14", "8", "16", "42", "48", "50"));
+        teams.add(new Team("Espanyol", "38", "13", "10", "15", "47", "51", "49"));
+        teams.add(new Team("Rayo Vallecano", "38", "15", "4", "19", "46", "68", "49"));
+        teams.add(new Team("Real Sociedad", "38", "11", "13", "14", "44", "51", "46"));
+        teams.add(new Team("Elche", "38", "11", "8", "19", "35", "62", "41"));
+        teams.add(new Team("Levante", "38", "9", "10", "19", "34", "67", "37"));
+        teams.add(new Team("Getafe", "38", "10", "7", "21", "33", "64", "37"));
+        teams.add(new Team("Deportivo", "38", "7", "14", "17", "35", "60", "35"));
+        teams.add(new Team("Granada", "38", "7", "14", "17", "29", "64", "35"));
+        teams.add(new Team("Eibar", "38", "9", "8", "21", "34", "55", "35"));
+        teams.add(new Team("Almeria", "38", "8", "8", "22", "35", "64", "32"));
+        teams.add(new Team("Cordoba", "38", "3", "11", "24", "22", "68", "20"));
 
         final Map<String, Object> data = new HashMap<>();
 
